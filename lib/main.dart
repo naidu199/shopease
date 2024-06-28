6327bdb2cfd303c47c4122ed3e7a9f77b0c9e749
K T Narasimha Naidu <141550407+naidu199@users.noreply.github.com>
2024-06-28 14:05:34 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Shopease',
       theme: ThemeData(
         
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
